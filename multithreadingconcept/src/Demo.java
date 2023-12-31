d53cc0e6fc2c6f3ac1ebf9759b358662945fd3a3
saurav123anand <123sauravanand@gmail.com>
2024-01-01 00:01:05 +0530

    threads implementation added

--- multithreadingconcept/src/Demo.java
+++ multithreadingconcept/src/Demo.java
@@ -1,8 +1,20 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
+        System.out.println("main thread is running ");
+        Thread thread=new Thread(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("java 8 syntax thread");
+        },"java 8");
+        thread.start();
+        System.out.println("main thread ended execution ");
+
 
     }
 }
