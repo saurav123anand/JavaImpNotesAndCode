89435c909babb10297ad1bb09ed89c9e91929a9a
GeekySaurav <65071308+saurav123anand@users.noreply.github.com>
2025-08-30 02:59:14 +0530

    Update Ques20.java

--- java8Questions/src/Ques20.java
+++ java8Questions/src/Ques20.java
@@ -6,13 +6,24 @@ import java.util.stream.Collectors;
 
 public class Ques20 {
     //Print duplicate characters in a string?
-    public static Set<String> duplicates(String str){
+    /**public static Set<String> duplicates(String str){
         Set<String> set=new HashSet<>();
         Set<String> result = Arrays.stream(str.split("")).filter(num -> !set.add(num)).collect(Collectors.toSet());
         return result;
+    }**/
+
+   public static Set<Character> findDuplicatesChars(String str) {
+        Set<Character> seen = new HashSet<>();
+        return str.chars()
+                .mapToObj(c -> (char) c)
+                .filter(ch -> !seen.add(ch))
+                .collect(Collectors.toSet());
     }
+
+
     public static void main(String[] args) {
         String inputString = "java is a awesome programming language";
-        System.out.println(duplicates(inputString));
+        // System.out.println(duplicates(inputString));
+        System.out.println(findDuplicatesChars(inputString));
     }
 }
